a0dd1113503dc7f4a8fc0d5163dcdf61303bce8f
furqaan ismail <furqaanismail.labfik@umi.ac.id>
2021-10-21 20:03:32 +0800

    update service

---
+++ src/main/java/com/example/springcrud/services/ProductService.java
@@ -0,0 +1,37 @@
+package com.example.springcrud.services;
+
+import com.example.springcrud.models.entities.Product;
+import com.example.springcrud.models.repository.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class ProductService {
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    public Product save(Product product){
+        return productRepo.save(product);
+    }
+
+    public Product findOne(Long id){
+        Optional<Product> product = productRepo.findById(id);
+        if(!product.isPresent()){
+            return null;
+        }
+        return product.get();
+    }
+
+    public Iterable<Product> findAll(){
+        return productRepo.findAll();
+    }
+
+    public void removeOne(Long id){
+        productRepo.deleteById(id);
+    }
+}
